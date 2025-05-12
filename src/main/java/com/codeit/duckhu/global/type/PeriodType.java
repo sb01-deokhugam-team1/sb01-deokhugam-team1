dd1ad306af6ec2058b1c994157cb127b20be4e35
byeonheejae <pion0458@gmail.com>
2025-05-12 14:25:56 +0900

    수정

--- src/main/java/com/codeit/duckhu/global/type/PeriodType.java
+++ src/main/java/com/codeit/duckhu/global/type/PeriodType.java
@@ -11,15 +11,9 @@ public enum PeriodType {
   ALL_TIME;
 
   public Instant toStartInstant(Instant now) {
-    ZoneId zoneId = ZoneId.of("Asia/Seoul");
-    ZonedDateTime nowInKST = now.atZone(zoneId);
-    
+
     return switch (this) {
-      case DAILY -> {
-        // 현재 날짜의 자정(00:00:00)을 기준으로 계산
-        ZonedDateTime startOfDay = nowInKST.toLocalDate().atStartOfDay(zoneId);
-        yield startOfDay.toInstant();
-      }
+      case DAILY -> now.minusSeconds(60 * 60 * 24); // 1일 전
       case WEEKLY -> now.minusSeconds(60 * 60 * 24 * 7); // 1주일 전
       case MONTHLY -> now.minusSeconds(60L * 60 * 24 * 30); // 30일 전
       case ALL_TIME -> Instant.EPOCH; // 올타임
